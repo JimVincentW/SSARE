5068ce39784c2dceff1549b2e7e771487df97a18
Jim Wagner <jimvwagner@googlemail.com>
2024-03-02 17:10:08 +0100

    added bbc

--- SSARE/scraper_service/scrapers/bbc.py
+++ SSARE/scraper_service/scrapers/bbc.py
@@ -43,6 +43,7 @@ async def main():
         article_urls = extract_article_urls(news_page_html)
         tasks = [process_article_url(session, base_url, url) for url in article_urls]
         articles = await asyncio.gather(*tasks)
+        df.to_csv('/app/scrapers/data/dataframes/bbc_articles.csv', index=False)
         df = pd.DataFrame(articles)
 
         # Show the DataFrame head
